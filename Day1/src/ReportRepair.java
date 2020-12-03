ff71805cdb005d59d6d21690b61f1e793c0da995
hadi <hadi.latef@gmail.com>
2020-12-03 23:58:35 +0000

    Day 1 advanced solution complete

--- Day1/src/ReportRepair.java
+++ Day1/src/ReportRepair.java
@@ -5,16 +5,12 @@ import java.util.Scanner;
 
 public class ReportRepair {
     public static void main(String[] args) throws FileNotFoundException {
-        findResult();
+        //findResultSimple();
+        findResultComplex();
     }
 
-    private static void findResult() throws FileNotFoundException {
-        ArrayList<Integer> reportList = new ArrayList<Integer>();
-        try (Scanner scanner = new Scanner(new FileReader("/git_clones/AdventOfCode/ReportRepairInput"))) {
-            while(scanner.hasNext()) {
-                reportList.add(Integer.parseInt(scanner.next()));
-            }
-        }
+    private static void findResultSimple() throws FileNotFoundException {
+        ArrayList<Integer> reportList = readFile();
         for (int i = 0; i < reportList.size()-1; i++) {
             for (int j = i + 1; j < reportList.size(); j++ ) {
                 if (reportList.get(i) + reportList.get(j) == 2020) {
@@ -22,5 +18,28 @@ public class ReportRepair {
                 }
             }
         }
+    }
+    //TODO: There has to be a better way to do this than brute force, work it out?
+    private static void findResultComplex() throws FileNotFoundException {
+        ArrayList<Integer> reportList = readFile();
+        for (int i = 0; i < reportList.size()-2; i++) {
+            for (int j = i + 1; j < reportList.size() - 1; j++) {
+                for (int k = j + 1; k < reportList.size(); k++) {
+                    if(reportList.get(i) + reportList.get(j) + reportList.get(k) == 2020) {
+                        System.out.println(reportList.get(i) * reportList.get(j) * reportList.get(k));
+                    }
+                }
+            }
+        }
+    }
+
+    private static ArrayList<Integer> readFile() throws FileNotFoundException {
+        ArrayList<Integer> reportList = new ArrayList<Integer>();
+        try (Scanner scanner = new Scanner(new FileReader("/git_clones/AdventOfCode/ReportRepairInput"))) {
+            while(scanner.hasNext()) {
+                reportList.add(Integer.parseInt(scanner.next()));
+            }
+        }
+        return reportList;
     }
 }
